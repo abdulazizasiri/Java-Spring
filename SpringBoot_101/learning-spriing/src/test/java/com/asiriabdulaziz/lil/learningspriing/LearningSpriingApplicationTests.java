9717236bfc552e8707f33649543aa794858f2f4b
abdulazizasiri <asiriaabdulaziz@gmail.com>
2021-05-09 17:44:39 +0300

    Sprint boot app

---
+++ SpringBoot_101/learning-spriing/src/test/java/com/asiriabdulaziz/lil/learningspriing/LearningSpriingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asiriabdulaziz.lil.learningspriing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpriingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
