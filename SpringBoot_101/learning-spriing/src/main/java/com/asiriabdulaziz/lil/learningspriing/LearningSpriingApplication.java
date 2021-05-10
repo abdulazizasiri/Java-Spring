2e9ed81ee7df158d05e3afe8e06c558222701c63
abdulazizasiri <asiriaabdulaziz@gmail.com>
2021-05-10 15:15:21 +0300

    lil spring

--- SpringBoot_101/learning-spriing/src/main/java/com/asiriabdulaziz/lil/learningspriing/LearningSpriingApplication.java
+++ SpringBoot_101/learning-spriing/src/main/java/com/asiriabdulaziz/lil/learningspriing/LearningSpriingApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LearningSpriingApplication {
 	public static void main(String[] args) {
-
-			SpringApplication.run(LearningSpriingApplication.class, args);
+		SpringApplication.run(LearningSpriingApplication.class, args);
 	}
 
 }
