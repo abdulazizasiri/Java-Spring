42c623bf4e3e83b6b8eb324232d30233564ab686
abdulazizasiri <asiriaabdulaziz@gmail.com>
2021-05-12 15:21:26 +0300

    created spring controller

---
+++ JavaBrains/newMAvenProject_1/src/main/java/springlil/topics/TopicController.java
@@ -0,0 +1,9 @@
+package springlil.topics;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TopicController {
+    
+}
---
+++ JavaBrains/newMAvenProject_1/src/main/java/springlil/topics/Topics.java
@@ -0,0 +1,4 @@
+package springlil.topics;
+
+public class Topics {
+}
