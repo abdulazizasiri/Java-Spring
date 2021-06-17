b938dc5741a4f5906bb571d16abce9233589e81d
abdulazizasiri <asiriaabdulaziz@gmail.com>
2021-06-17 10:57:46 +0300

    spring boot project

---
+++ SpringBoot_101/springBootEssentials/booting-web/src/test/java/com/asiri/lil/sbnet/bootingweb/BootingWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asiri.lil.sbnet.bootingweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootingWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
