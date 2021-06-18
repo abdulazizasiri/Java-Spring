10ffbb0cdcd72eac6676d9b1c14fbc50f332ba97
abdulazizasiri <asiriaabdulaziz@gmail.com>
2021-06-18 15:08:10 +0300

    amigo cool intro

--- SpringBoot_101/AmigoSpring/amigocourse/src/main/java/com/aziz/amigo/amigocourse/AmigocourseApplication.java
+++ SpringBoot_101/AmigoSpring/amigocourse/src/main/java/com/aziz/amigo/amigocourse/AmigocourseApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AmigocourseApplication {
 
 	public static void main(String[] args) {
+		// Main method.  Starting 
 		SpringApplication.run(AmigocourseApplication.class, args);
 	}
 
