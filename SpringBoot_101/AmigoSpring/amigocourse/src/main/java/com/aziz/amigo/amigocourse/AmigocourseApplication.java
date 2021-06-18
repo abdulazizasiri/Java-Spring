e394da578b73d4628d4e595a74aaa0fe38bb2493
abdulazizasiri <asiriaabdulaziz@gmail.com>
2021-06-18 13:54:19 +0300

    created a new spring project.

---
+++ SpringBoot_101/AmigoSpring/amigocourse/src/main/java/com/aziz/amigo/amigocourse/AmigocourseApplication.java
@@ -0,0 +1,13 @@
+package com.aziz.amigo.amigocourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmigocourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmigocourseApplication.class, args);
+	}
+
+}
---
+++ SpringBoot_101/AmigoSpring/amigocourse/src/test/java/com/aziz/amigo/amigocourse/AmigocourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aziz.amigo.amigocourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmigocourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
