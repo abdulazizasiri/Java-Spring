80d698ae5fe662d94b53a8c1db80bf8426c512d8
abdulazizasiri <asiriaabdulaziz@gmail.com>
2021-07-09 14:04:48 +0300

    Spring boot starter project

---
+++ JavaBrains/newProject_V2/ccoursesAPIv2/src/main/java/com/asiri/courses/ccoursesAPIv2/CoursesApIv2Application.java
@@ -0,0 +1,13 @@
+package com.asiri.courses.ccoursesAPIv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursesApIv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursesApIv2Application.class, args);
+	}
+
+}
---
+++ JavaBrains/newProject_V2/ccoursesAPIv2/src/test/java/com/asiri/courses/ccoursesAPIv2/CcoursesApIv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asiri.courses.ccoursesAPIv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcoursesApIv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
