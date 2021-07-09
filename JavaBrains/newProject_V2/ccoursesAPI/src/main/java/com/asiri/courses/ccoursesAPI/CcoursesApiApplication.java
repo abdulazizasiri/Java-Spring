d63dc7dc2d6a00deec7933d181c526a5dd943026
abdulazizasiri <asiriaabdulaziz@gmail.com>
2021-07-09 13:25:14 +0300

    added a new spring project

---
+++ JavaBrains/newProject_V2/ccoursesAPI/src/main/java/com/asiri/courses/ccoursesAPI/CcoursesApiApplication.java
@@ -0,0 +1,13 @@
+package com.asiri.courses.ccoursesAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcoursesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcoursesApiApplication.class, args);
+	}
+
+}
---
+++ JavaBrains/newProject_V2/ccoursesAPI/src/test/java/com/asiri/courses/ccoursesAPI/CcoursesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asiri.courses.ccoursesAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcoursesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
