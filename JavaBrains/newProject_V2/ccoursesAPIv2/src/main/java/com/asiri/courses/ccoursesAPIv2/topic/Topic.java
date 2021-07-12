5fabc234e3251dd293bfbf1449f57f57977f06bc
abdulazizasiri <asiriaabdulaziz@gmail.com>
2021-07-12 21:57:24 +0300

    started a new repository

--- JavaBrains/newProject_V2/ccoursesAPIv2/src/main/java/com/asiri/courses/ccoursesAPIv2/topic/Topic.java
+++ JavaBrains/newProject_V2/ccoursesAPIv2/src/main/java/com/asiri/courses/ccoursesAPIv2/topic/Topic.java
@@ -1,6 +1,14 @@
 package com.asiri.courses.ccoursesAPIv2.topic;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Topic {
+
+    @Id
+    @GeneratedValue
     private String id;
     private String name;
     private String description ;
---
+++ JavaBrains/newProject_V2/ccoursesAPIv2/src/main/java/com/asiri/courses/ccoursesAPIv2/topic/TopicRepository.java
@@ -0,0 +1,11 @@
+package com.asiri.courses.ccoursesAPIv2.topic;
+
+public interface TopicRepository {
+
+    // getAllTopics()
+    // getTopic(id)
+    // updateTopic(Topic t)
+    // deleteTopic(id)
+
+
+}
