83ce97dee92e37c1b21e856a4b5de4f05e62c9cd
abdulazizasiri <asiriaabdulaziz@gmail.com>
2021-07-18 16:37:03 +0300

    smigo project

---
+++ AmigoSpringBoot/amigoProject/src/main/java/com/example/amigoandaziz/amigoProject/AmigoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.amigoandaziz.amigoProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmigoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmigoProjectApplication.class, args);
+	}
+
+}
---
+++ AmigoSpringBoot/amigoProject/src/test/java/com/example/amigoandaziz/amigoProject/AmigoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amigoandaziz.amigoProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmigoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
