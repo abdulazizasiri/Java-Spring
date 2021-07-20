54b1148d79f165c41d779ff8f0d232a897978807
abdulazizasiri <asiriaabdulaziz@gmail.com>
2021-07-20 16:46:43 +0300

    created the 4 packages, Dao, service, model, api

---
+++ AmigoSpringBoot/amigoProject/src/main/java/com/example/amigoandaziz/amigoProject/dao/PersonDao.java
@@ -0,0 +1,13 @@
+package com.example.amigoandaziz.amigoProject.dao;
+
+import com.example.amigoandaziz.amigoProject.model.Person;
+
+import java.util.UUID;
+
+public interface PersonDao {
+    int insertPerson(UUID id, Person person) ;
+    default int insertPerson(Person person) {
+        UUID id = UUID.randomUUID();
+        return insertPerson(id, person);
+    }
+}
