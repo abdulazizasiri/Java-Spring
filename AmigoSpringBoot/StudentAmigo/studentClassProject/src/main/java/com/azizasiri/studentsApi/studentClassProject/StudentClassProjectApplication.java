a32f4d218c9aa747e9cefaae299015d5a27c2bde
abdulazizasiri <asiriaabdulaziz@gmail.com>
2021-07-28 22:14:50 +0300

    created the third projecct

---
+++ AmigoSpringBoot/StudentAmigo/studentClassProject/src/main/java/com/azizasiri/studentsApi/studentClassProject/StudentClassProjectApplication.java
@@ -0,0 +1,13 @@
+package com.azizasiri.studentsApi.studentClassProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentClassProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentClassProjectApplication.class, args);
+	}
+
+}
---
+++ AmigoSpringBoot/StudentAmigo/studentClassProject/src/test/java/com/azizasiri/studentsApi/studentClassProject/StudentClassProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azizasiri.studentsApi.studentClassProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentClassProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
