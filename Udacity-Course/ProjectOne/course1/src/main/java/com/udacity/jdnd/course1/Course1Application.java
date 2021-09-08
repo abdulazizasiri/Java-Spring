2cfc9416d049e0540c41d58c81b27425c7e1ae5f
abdulazizasiri <asiriaabdulaziz@gmail.com>
2021-09-08 09:30:26 +0300

    project set up

--- Udacity-Course/ProjectOne/course1/src/main/java/com/udacity/jdnd/course1/Course1Application.java
+++ Udacity-Course/ProjectOne/course1/src/main/java/com/udacity/jdnd/course1/Course1Application.java
@@ -1,9 +1,14 @@
 package com.udacity.jdnd.course1;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@ComponentScan
+@Configuration
+@EnableAutoConfiguration
 public class Course1Application {
 
 	public static void main(String[] args) {
