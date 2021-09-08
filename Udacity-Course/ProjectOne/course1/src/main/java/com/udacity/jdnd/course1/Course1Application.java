f9fa966ac4cafaead1d40c8a6c805c0b9eedd975
abdulazizasiri <asiriaabdulaziz@gmail.com>
2021-09-08 09:56:54 +0300

    project one

--- Udacity-Course/ProjectOne/course1/src/main/java/com/udacity/jdnd/course1/Course1Application.java
+++ Udacity-Course/ProjectOne/course1/src/main/java/com/udacity/jdnd/course1/Course1Application.java
@@ -3,16 +3,23 @@ package com.udacity.jdnd.course1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@ComponentScan
-@Configuration
+@SpringBootApplication
+// This annotation is 3 other combined annotations: @Configuration, @ComponentScan,  @EnableAutoConfiguration
 @EnableAutoConfiguration
 public class Course1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Course1Application.class, args);
+	}
+
+	@Bean
+	public String nameServices(){
+		System.out.println("Hello all"); //
+		return "Hello all";
 	}
 
 }
