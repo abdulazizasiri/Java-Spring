400d459d5dc3b2422baa8506e8a0efaa74c37674
abdulazizasiri <asiriaabdulaziz@gmail.com>
2021-09-23 17:49:14 +0300

    graphql app

---
+++ Udacity-Course/Core_3_Web_Services/graphqlApp/graphqlapp/src/test/java/com/udacity/graphql/graphqlapp/GraphqlappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udacity.graphql.graphqlapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
