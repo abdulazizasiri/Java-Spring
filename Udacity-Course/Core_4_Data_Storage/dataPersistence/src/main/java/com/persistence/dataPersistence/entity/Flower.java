7a4621a63173211b8308c8416c9409d210dfe74d
abdulazizasiri <asiriaabdulaziz@gmail.com>
2021-10-02 17:37:41 +0300

    made two entites for the project

---
+++ Udacity-Course/Core_4_Data_Storage/dataPersistence/src/main/java/com/persistence/dataPersistence/entity/Flower.java
@@ -0,0 +1,22 @@
+package com.persistence.dataPersistence.entity;
+
+import org.hibernate.annotations.Nationalized;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "plant")
+public class Flower {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Nationalized // should use @Nationalized instead of @Type=nstring
+    private String name;
+    private String color;
+    @Column(precision=12, scale=4)
+    private BigDecimal price; // BigDecimal is the standard Java class for currency math
+
+    /* getters and setters*/
+}
