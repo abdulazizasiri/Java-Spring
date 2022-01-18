98ced87ebd41b6487b7ea57cc9715a43ff2f8722
abdulazizasiri <asiriaabdulaziz@gmail.com>
2022-01-18 10:43:46 +0300

    created a table

--- Udemy/BeginnerToGuru/spring5webapp/src/main/java/com/aziz/learningspring/spring5webapp/model/Book.java
+++ Udemy/BeginnerToGuru/spring5webapp/src/main/java/com/aziz/learningspring/spring5webapp/model/Book.java
@@ -1,2 +1,13 @@
-package com.aziz.learningspring.spring5webapp.model;public class Book {
+package com.aziz.learningspring.spring5webapp.model;
+
+import java.util.Set;
+
+public class Book {
+    private String title;
+    private String isbn;
+    private Set<Author> authorSet;
+
+    //  No arg constructor is required in JPA
+    public Book() {
+    }
 }
