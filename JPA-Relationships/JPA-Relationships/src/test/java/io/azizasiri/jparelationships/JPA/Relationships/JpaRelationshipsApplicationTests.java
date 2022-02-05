a76a94eacbaf868f60f8bbc4208e1320ce3524af
Abdulazizasiri <asiriaabdulaziz@gmail.com>
2022-02-05 16:29:02 +0300

    jpa relationships

---
+++ JPA-Relationships/JPA-Relationships/src/test/java/io/azizasiri/jparelationships/JPA/Relationships/JpaRelationshipsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.azizasiri.jparelationships.JPA.Relationships;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaRelationshipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
