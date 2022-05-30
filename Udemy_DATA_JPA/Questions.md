46ca8463ed1185eb6433269454cb4e46944674c4
abdulazizasiri <asiriaabdulaziz@gmail.com>
2022-05-30 20:10:55 +0300

    spring data jpa

---
+++ Udemy_DATA_JPA/Questions.md
@@ -0,0 +1,13 @@
+### Questions regarding Spring Data
+
+- What is Spring data? 
+
+- What is the difference between Spring Data JPA and Hibernate? 
+
+- What is a data store in spring? (the types of database i think: sql vs nosql)
+
+
+- What is the difference between Spring Data JPA and JPA? 
+
+
+
---
+++ Udemy_DATA_JPA/section_2/Spring-Data-JPS/src/main/java/com/springdatajpa/jpa/Spring/Data/JPS/SpringDataJpsApplication.java
@@ -0,0 +1,13 @@
+package com.springdatajpa.jpa.Spring.Data.JPS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpsApplication.class, args);
+	}
+
+}
