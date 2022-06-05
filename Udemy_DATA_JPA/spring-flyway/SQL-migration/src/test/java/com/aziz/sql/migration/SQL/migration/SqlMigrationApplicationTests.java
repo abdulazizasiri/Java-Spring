edddaacfe61b4a688432f16d71d0ac8dea1928c4
abdulazizasiri <asiriaabdulaziz@gmail.com>
2022-06-05 22:37:31 +0300

    started flyway

---
+++ Udemy_DATA_JPA/spring-flyway/SQL-migration/src/test/java/com/aziz/sql/migration/SQL/migration/SqlMigrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aziz.sql.migration.SQL.migration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlMigrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
